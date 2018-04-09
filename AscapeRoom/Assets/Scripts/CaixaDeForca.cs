cedeed250ef86bbc7d186490d635ab6cb9ab396e
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2018-04-09 10:40:17 -0300

    Interfaces de interação

---
+++ AscapeRoom/Assets/Scripts/CaixaDeForca.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaixaDeForca : MonoBehaviour, IInteractive
+{
+    void Start ()
+    {
+	    	
+	}
+	void Update ()
+    {
+		
+	}
+    public void Inspect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact(IItem item)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ AscapeRoom/Assets/Scripts/Manager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public IItem Active;
+    public List<IItem> items;
+
+    //o inventario ficaria aqui
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                ray = Camera.main.ScreenPointToRay(touch.position);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+        }
+        if (Physics.Raycast(ray, out hit))
+        {
+            TryToInteract(hit);
+        }
+    }
+    void TryToInteract(RaycastHit hit)
+    {
+        IInteractive interactive = hit.transform.gameObject.GetComponent<IInteractive>();
+        if (interactive != null)
+        {
+            if( Active != null )
+            {
+                interactive.Inspect();
+            }
+            else
+            {
+                interactive.Interact(Active);
+            }
+        }
+    }
+    void EquipItem(IItem toEquip)
+    {
+        Active = toEquip;
+    }
+    void UnequipItem()
+    {
+        Active = null;
+    }
+}
