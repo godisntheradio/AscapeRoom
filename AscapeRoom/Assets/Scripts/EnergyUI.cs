caa29fe1cf5008eef4065c3acabc4dc40e6df540
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2018-04-28 00:07:49 -0300

    Implementação de ligar/desligar energia

---
+++ AscapeRoom/Assets/Scripts/EnergyUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyUI : MonoBehaviour
+{
+    bool Changed;
+    [SerializeField]
+    Texture On;
+    [SerializeField]
+    Texture Off;
+
+    UnityEngine.UI.RawImage imageRef;
+    void Start ()
+    {
+        Changed = Manager.instance.HasEnergy;
+        imageRef = GetComponent<UnityEngine.UI.RawImage>();
+	}
+	
+	void Update ()
+    {
+        if (Manager.instance.HasEnergy != Changed)
+        {
+            if(Manager.instance.HasEnergy)
+            {
+                imageRef.texture = On;
+            }
+            else
+            {
+                imageRef.texture = Off;
+            }
+            Changed = Manager.instance.HasEnergy;
+        }
+	}
+}
